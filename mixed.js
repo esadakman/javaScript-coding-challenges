15f9d3ac471d6ec7ef3165d33ed57e52c939cc2d
esadakman <98649983+esadakman@users.noreply.github.com>
2022-06-10 21:51:43 +0300

    javaScript-coding-challenges created

---
+++ mixed.js
@@ -0,0 +1,178 @@
+// ! Quetion 1____________________________________________
+// You have a two-dimensional list in the following format:
+
+// data = [[2, 5], [3, 4], [8, 7]]
+// Each sub-list contains two items, and each item in the sub-lists is an integer.
+
+// Write a function process_data()/processData() that processes each sub-list like so:
+
+// [2, 5] --> 2 - 5 --> -3
+// [3, 4] --> 3 - 4 --> -1
+// [8, 7] --> 8 - 7 --> 1
+// and then returns the product of all the processed sub-lists: -3 * -1 * 1 --> 3.
+
+// data = [[2, 5], [3, 4], [8, 7]]
+
+// let multi = 1;
+// let empty_list = []
+
+// for (var x of data){
+//     // ! multi *= x[0]-x[1];
+//     let fat = x[0] - x[1];
+//     empty_list.push(fat)
+
+// }
+// for (let y of empty_list) {
+//   multi *= y;
+// }
+// console.log(multi);
+
+// ! Quetion 2____________________________________________
+
+// Compute the running median of a sequence of numbers. That is, given a stream of numbers, print out the median of the list so far on each new element.
+
+// Recall that the median of an even-numbered list is the average of the two middle numbers.
+
+// For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should print out: 2, 1.5, 2, 3.5, 2, 2, 2
+
+// let median = (arr) => {
+//   newArr = [];
+//   newArr2 = [];
+//   for (i = 0; i < arr.length; i++) {
+//     newArr.push(arr[i]);
+//     newArr = newArr.sort((a, b) => (a > b ? 1 : -1));
+//     let middle = Math.floor(newArr.length / 2);
+//     if (newArr.length % 2 == 0) {
+//       newArr2.push((newArr[middle - 1] + newArr[middle]) / 2);
+//     } else {
+//       newArr2.push(newArr[middle]);
+//     }
+//   }
+//   return newArr2;
+// };
+
+// ! Quetion 3____________________________________________
+// ? This is an interview question asked by Uber.
+// Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+// For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+// const uber = (arr) => {
+//   let newArr = [];
+//   let multi = arr.reduce((x, y) => x * y);
+//   for (i = 0; i < arr.length; i++) {
+//     newArr.push(multi / arr[i]);
+//   }
+//   return newArr;
+// };
+
+// console.log(uber([10, 5, 9])); // [45, 90, 50]
+
+// ! Quetion 5____________________________________________
+// ? // TASK:
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+// Implement a function that determines whether a string that contains only letters
+// is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// const isogram = (str) => {
+//     let strArr = str.toLowerCase();
+//     strArr = strArr.split("");
+//     let unique = strArr.filter((x, y, z) => z.indexOf(x) === y);
+//     return str.length != unique.length ? false : true;
+//   };
+
+//   a = "Dermatoglyphics";
+
+//   console.log(isogram("DermatoglyphicsSq"));
+
+// ! Quetion 6____________________________________________
+// ? Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed. Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// const reversedFive = (str) => {
+//   let words = str.split(" ");
+//   let filtered = words
+//     .map((i) => {
+//       return i.length > 5 ? i.split("").reverse().join("") : i;
+//     })
+//     .join(" ");
+//   return filtered;
+// };
+
+// console.log(reversedFive("asdasddz asd a"));
+
+// This problem was asked by Microsoft.
+
+// ! Quetion 7____________________________________________
+// ? Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road ("_") or bumps ("n"), work out if you make it home safely. 15 bumps or under, return "Woohoo!", over 15 bumps return "Car Dead".
+
+// Samples:
+// bump("_nnnnnnn_n__n______nn__nn_nnn") => "Car Dead"
+// bump("______n___n_") => "Woohoo!"
+
+// const carCond = (str) => {
+//   let bumpy = str.split("n").length - 1;
+//   return bumpy >= 15 ? "Car Dead" : "Woohoo!";
+// };
+
+// console.log(carCond("_nnnnnnn_n__n______nn__nn_nnn"));
+// console.log(carCond("______n___n_"));
+
+// ! Quetion 8____________________________________________
+// ? There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+const findUniq = (arr) => {
+  arr = arr.sort((x, y) => x - y);
+  return arr[0] == arr[1] ? arr[arr.length - 1] : arr[0];
+};
+
+// return bumpy >= 15 ? "Car Dead" : "Woohoo!";
+
+// console.log(findUniq([1, 1, 2, 1, 1, 1]));
+// console.log(findUniq([1, 3, 3, 3, 3, 3]));
+// console.log(findUniq([0, 0, 0.55, 0, 0]));
+
+// ! Quetion 9____________________________________________
+// ? Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that all the Rs come first, the Gs come second, and the Bs come last. You can only swap elements of the array.
+
+// Do this in linear time and in-place.
+
+// For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+
+// const rgb = (arr) => {
+// for (i = 0; i < arr.length; i++) {
+//   if (arr[i] = "R") {
+
+//   }
+
+// }
+// arr.unshift(1);
+// for (i = 0; i < arr.length; i++) {
+// if (arr[arr.length - 1 == 1]) {
+// return arr;
+// } else {
+// if ((arr[i] = "R")) {
+// arr.slice(arr[i]);
+// arr.push(arr[i]);
+// }
+// }
+// arr.reduce((x,y,z) => )
+// }
+// };
+
+// console.log(rgb(["G", "B", "R", "R", "B", "R", "G"]));
+
+// ! Quetion 10____________________________________________
+// ? Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Samples:
+// 16  -->  1 + 6 = 7
+// 942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+const digits = (arr) => {
+  let bumpy = arr.split("n").length - 1;
+  return bumpy;
+};
+
+console.log(digits([1234]));
