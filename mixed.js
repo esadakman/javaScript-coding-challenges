d572b6bf84e02a1f65ef5797d840fbc1e1f2ac3a
esadakman <98649983+esadakman@users.noreply.github.com>
2022-06-12 00:05:48 +0300

    Q.11 isWalidWalk solution added

--- mixed.js
+++ mixed.js
@@ -176,3 +176,48 @@ digits(16); // 7
 digits(942); // 6
 digits(132189); // 6
 digits(493193); // 2
+
+// ! Quetion 11____________________________________________
+// ? You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// samples:
+// isValidWalk(['n','s','n','s','n','s','n','s','n','s']) => true
+// isValidWalk(['w']) => false
+// isValidWalk(['n','n','n','s','n','s','n','s','n','s']) => false
+// isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']) => false
+
+// const isValidWalk = (arr) => {
+//   var result = arr.reduce(function (x, y) {
+//     return (x[y] = (x[y] || 0) + 1), x;
+//   }, []);
+//   return result;
+//   if ((result.n == 5 && result.s == 5) || (result.w == 5 && result.e == 5)) {
+// return true;
+//   } else {
+// return false;
+//   }
+// return (result.n == 5 && result.s == 5) || (result.w == 5 && result.e == 5)
+//   ? true
+//   : false;
+// };
+
+const isValidWalk = (arr) => {
+  var result = arr.reduce((x, y) => {
+    if (y in x) {
+      x[y]++;
+    } else {
+      x[y] = 1;
+    }
+    return x;
+  }, []);
+  return (result.n == 5 && result.s == 5) || (result.w == 5 && result.e == 5)
+    ? true
+    : false;
+};
+
+console.log(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
+console.log(isValidWalk(["w"]));
+console.log(isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]));
+console.log(isValidWalk(["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"]));
+
+
