a60417e2aa06767dd712468cc783156f9549193d
esadakman <98649983+esadakman@users.noreply.github.com>
2022-06-17 00:02:52 +0300

    pair of gloves added

--- mixed.js
+++ mixed.js
@@ -258,3 +258,29 @@ function divisors(num) {
 console.log(divisors(12)); // [2,3,4,6]
 console.log(divisors(25)); // [5]
 console.log(divisors(13)); // "13 is prime"
+
+// ! Quetion 13____________________________________________
+// ? Determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+// ?Given an array describing the color of each glove, return the number of pairs you can constitute, assuming that only gloves of the same color can form pairs.
+
+// examples:
+// input = ["red", "green", "red", "blue", "blue"]
+// result = 2     because (1 red pair + 1 blue pair)
+
+// input = ["red", "red", "red", "red", "red", "red"]
+// result = 3     because (3 red pairs)
+
+const numOfPairs = (arr) => {
+  var gloves = arr.reduce(function (x, y) {
+    return (x[y] = (x[y] || 0) + 1), x;
+  }, []);
+  let count = 0;
+  for (pair in gloves) {
+    count += Math.floor(gloves[pair] / 2);
+  }
+
+  return count;
+};
+
+console.log(numOfPairs(["red", "green", "red", "blue", "blue"]));
+console.log(numOfPairs(["red", "red", "red", "red", "red", "red"]));
