5bc45cd3b42dd3c2d51d3eb49859455bbb191ae1
esadakman <98649983+esadakman@users.noreply.github.com>
2022-06-16 23:58:23 +0300

    divisors added

--- mixed.js
+++ mixed.js
@@ -237,3 +237,24 @@ function missingLetter(arr) {
 
 console.log(missingLetter(["a", "b", "c", "d", "f"])); // "e"
 console.log(missingLetter(["O", "Q", "R", "S"])); // "e"
+
+// ! Quetion 13____________________________________________
+// ? Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime'
+
+// Examples:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+function divisors(num) {
+  arr = [];
+  for (i = 2; i < num; i++) {
+    if (num % i === 0) {
+      arr.push(i);
+    }
+  }
+  return arr.length != 0 ? arr : `${num} is prime`;
+}
+console.log(divisors(12)); // [2,3,4,6]
+console.log(divisors(25)); // [5]
+console.log(divisors(13)); // "13 is prime"
