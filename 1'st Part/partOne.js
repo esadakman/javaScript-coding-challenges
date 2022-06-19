4f5c5d135e64ab2896e69ff3c1e5c9c4dfeb8f23
esadakman <98649983+esadakman@users.noreply.github.com>
2022-06-19 13:22:57 +0300

    solution 5 added

--- 1'st Part/partOne.js
+++ 1'st Part/partOne.js
@@ -83,3 +83,16 @@ const isogram = (str) => {
 };
 
 console.log(isogram("DermatoglyphicsSq")); // false
+
+// ! Quetion 5____________________________________________
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+function digitize(n) {
+  return String(n).split("").reverse().map(Number);
+}
+
+console.log(digitize(348597));
+
+
+
