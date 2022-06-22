8a7693de5bec3cbf0d5618c5c1e9e9d99fff8871
esadakman <98649983+esadakman@users.noreply.github.com>
2022-06-22 23:58:53 +0300

    sevenAte9 solved

--- 4'th Part/partFour.js
+++ 4'th Part/partFour.js
@@ -97,3 +97,25 @@ function toCamelCase(str) {
 // console.log(toCamelCase("the-stealth-warrior"));
 // console.log(toCamelCase("The_Stealth_Warrior"));
 
+// ! Quetion 20____________________________________________
+
+// ? Write a function that removes every lone 9 that is inbetween 7s.
+
+// "79712312" --> "7712312"
+// "79797"    --> "777"
+
+function sevenAte9(str) {
+  arr = str.split("");
+  for (let i = 1; i < arr.length - 1; i++) {
+    if (arr[i - 1] == "7" && arr[i] == "9" && arr[i + 1] == "7") {
+      arr.splice(i, 1);
+    }
+  }
+
+  return arr.join("");
+}
+
+console.log(sevenAte9("79712312"));
+console.log(sevenAte9("79797"));
+console.log(sevenAte9("165561786121789797"));
+
